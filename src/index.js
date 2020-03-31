f372469ba4a787cf66a7c4a767b4ddbadaeab2ac
Alex Dymov <a8dymov@gmail.com>
2020-03-31 16:50:14 +0500

    removed the redux persist from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import CartProvider from './providers/cart/cart.provider';
 
